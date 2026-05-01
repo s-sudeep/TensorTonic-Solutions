c235651264f3c8b155ba558fa029dc72ef669543
s-sudeep <sudeep.sheshadri@gmail.com>
2026-05-02 00:17:42 +0530

    Add solution for moving-median

---
+++ moving-median/moving-median.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def moving_median(values, window_size):
+    """
+    Compute the rolling median for each window position.
+    """
+    # Write code here
+    values = np.array(values)
+    ws = window_size
+    output = [ np.median(np.sort(values[i:i+ws])) for i in range(len(values) + 1 - ws) ]
+
+    return output
+    
