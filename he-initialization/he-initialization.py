98901fd938389a85fb566c29949d4fc1ce94574b
s-sudeep <sudeep.sheshadri@gmail.com>
2026-05-29 20:20:41 +0530

    Add solution for he-initialization

---
+++ he-initialization/he-initialization.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def he_initialization(W, fan_in):
+    """
+    Scale raw weights to He uniform initialization.
+    """
+    # Write code here
+    W = np.array(W)
+    L = np.sqrt(6 / fan_in)
+    return W * 2 * L - L
